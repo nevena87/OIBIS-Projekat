fc5b61d017a675ab3f1e0c284d0931b0b87178bb
nevena87 <culibrk.nevena@gmail.com>
2023-10-31 21:57:52 +0100

    manager

--- BackupService/Backup.cs
+++ BackupService/Backup.cs
@@ -1,7 +1,12 @@
-﻿using Common;
+﻿using CertificateManager;
+using Common;
+using Manager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
